8b06e9fe19eb77cadb167197c0ab77850bdcc535
shiba <rp.shiba2@gmail.com>
2024-03-07 20:07:45 +0530

    navigation shopping app

--- App.jsx
+++ App.jsx
@@ -1,34 +1,23 @@
-import {SafeAreaView, ScrollView} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import FlatCards from './componnets/FlatCards';
-import ElevatedCards from './componnets/ElevatedCards';
-import FancyCard from './componnets/FancyCard';
-import ActionCard from './componnets/ActionCard';
-import ContactList from './componnets/ContactList';
-import PasswordGenerator from './componnets/PasswordGenerator';
-import RollDice from './componnets/RollDice';
-import FlatListDemo from './componnets/FlatListDemo';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './componnets/Home';
+import Details from './componnets/Details';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <FlatCards />
-        <ElevatedCards />
-        <FancyCard />
-        <ActionCard />
-        <ContactList />
-        <PasswordGenerator />
-        <RollDice />
-      </ScrollView>
-
-      {/* flat list is never br nested under scrollview FlatList is preferred over
-      the basic ScrollView for displaying long lists of data because ScrollView
-      renders all of its child components at once, leading to performance issues
-      with large data sets. */}
-      {/* <FlatListDemo /> */}
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} options />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({});
