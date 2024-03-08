71067d8b44128eac4b3c63da70594304904e0c54
shiba <rp.shiba2@gmail.com>
2024-03-08 15:17:24 +0530

    todo with search

--- App.jsx
+++ App.jsx
@@ -1,19 +1,64 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+
 
 const App = () => {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleAddTodo = () => {
+    if (!todo) return;
+    setTodos([...todos, {id: Date.now(), text: todo}]);
+    setTodo('');
+  };
+
+  const handleRemoveTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const filteredTodos = todos.filter(todo =>
+    todo.text.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <View className=" mt-8 px-2 h-full w-full flex flex-col justify-center items-center bg-yellow-400">
-      <Text className="font-sans text-3xl text-red-800">
-        native wind working
-      </Text>
-      <Text className="font-sans text-3xl text-red-800">
-        native wind working 
-      </Text>
+    <View className="flex-1 bg-red-500 items-center pt-40">
+      <TextInput
+        className="border-1 border-gray-300 p-10 my-10 w-90%"
+        placeholder="Add a new todo"
+        value={todo}
+        onChangeText={setTodo}
+      />
+      <Button title="Add Todo" onPress={handleAddTodo} />
+
+      <TextInput
+        className="border-1 border-gray-300 p-10 my-10 w-90%"
+        placeholder="Search Todos"
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+      />
+
+      <FlatList
+        data={filteredTodos}
+        renderItem={({item}) => (
+          <View className="flex-row justify-between items-center w-90% my-5 p-10 border-1 border-gray-300">
+            <Text className="max-w-80%">{item.text}</Text>
+            <TouchableOpacity onPress={() => handleRemoveTodo(item.id)}>
+              <Text className="text-blue-500">Remove</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
